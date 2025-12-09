cf543212428e1ce65278bbf15edbef2c88a70c52
mustaque01 <mustaque01@example.com>
2025-12-09 22:21:40 +0530

    Fix Clients component by removing redundant SVG closing tag

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -106,7 +106,6 @@ const Clients = () => {
                     <svg key={i} className="w-6 h-6 fill-current text-yellow-400" viewBox="0 0 20 20">
                       <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
                     </svg>
-                    </svg>
                   ))}
                 </div>
               </div>
